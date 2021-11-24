b9d286c3c461136179b780f66f48dc323ba3b0fd
pranaavr <68622267+pranaavr@users.noreply.github.com>
2021-11-24 00:37:01 -0500

    password encryption method in class User created

--- src/User.java
+++ src/User.java
@@ -1,5 +1,33 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class User {
     String name;
-    String plaintext;
-    
+    String passwordToHash;
+
+    String get_SHA_256(String plaintext) {
+        //get salt
+        SecureRandom sr = new SecureRandom();
+        byte[] saltb = new byte[16];
+        sr.nextBytes(saltb);
+        String salt = saltb.toString();
+        
+        //encrypt
+        String encryptedPass = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(salt.getBytes());
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (int i=0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            encryptedPass = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return encryptedPass;
+    }
 }
