464a8eeab5e366754bb712b63904df1ffbc07a28
pranaavr <68622267+pranaavr@users.noreply.github.com>
2021-11-25 01:51:37 -0500

    password.txt creation and deletion

--- src/Program.java
+++ src/Program.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Program {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Boolean flag = true;
         User user1 = new User();
+        
+        File text = new File("passwords.txt");
+        System.out.println("File created: " + text.getName());
+
         System.out.println("New User: ");
         user1.setName(scan.nextLine());
         System.out.println("New Password: ");
-        
         String pass = scan.nextLine();
+
         if (user1.verifyPassword(pass)) {
             user1.setPasswordToHash(pass);
         }
@@ -18,10 +22,13 @@ public class Program {
             return;
         }
 
-
         System.out.println("Username: "+ user1.getName());
         System.out.println("Password: " + user1.getPasswordToHash());
         System.out.println(user1.getSHA256());
+
+
+        text.delete();
+        System.out.println("Deleted file: "+text.getName());
     }
 
 }
